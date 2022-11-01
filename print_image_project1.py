aa88d7a40695bc85d6bb983c079b5ef8bdff0930
Atomschorsch <a.klueber@mailbox.org>
2022-11-01 09:45:25 +0100

    Fix renaming

--- print_image_project1.py
+++ print_image_project1.py
@@ -6,16 +6,11 @@ import IPython.display as display
 import numpy as np
 import glob
 from helpers.visualization import visualize_tf_record_dataset
-from helpers.exploratory_analysis import inspect_tf_record_structure, print_dataset_basics
+from helpers.exploratory_analysis import store_tf_record_structure, show_dataset_basics
 
 
-def from_example(tf_record_path_array):
-    '''Function example from https://www.tensorflow.org/tutorials/load_data/tfrecord'''
-    raw_image_dataset = tf.data.TFRecordDataset(tf_record_path_array)
-    inspect_tf_record_structure(raw_image_dataset)
-    print_dataset_basics(raw_image_dataset)
-
-    # Create a dictionary describing the features.
+def parse_record(record):
+    '''Function to parse one record.'''
     image_feature_description = {
         'image/height': tf.io.FixedLenFeature([], tf.int64),
         'image/width': tf.io.FixedLenFeature([], tf.int64),
@@ -31,21 +26,37 @@ def from_example(tf_record_path_array):
         'image/object/class/text': tf.io.VarLenFeature(dtype=tf.string),
         'image/object/class/label': tf.io.VarLenFeature(dtype=tf.int64),
     }
+    return tf.io.parse_single_example(record, image_feature_description)
 
-    def _parse_image_function(example_proto):
-        # Parse the input tf.train.Example proto using the dictionary above.
-        return tf.io.parse_single_example(example_proto, image_feature_description)
 
-    parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+def from_example(tf_record_path_array):
+    '''Function example from https://www.tensorflow.org/tutorials/load_data/tfrecord'''
+    raw_image_dataset = tf.data.TFRecordDataset(tf_record_path_array)
+    store_tf_record_structure(raw_image_dataset)
+    show_dataset_basics(raw_image_dataset)
+
+    # # Create a dictionary describing the features.
+    # image_feature_description = {
+    #   'image/height': tf.io.FixedLenFeature([], tf.int64),
+    #   'image/width': tf.io.FixedLenFeature([], tf.int64),
+    #   'image/filename': tf.io.FixedLenFeature([], tf.string),
+    #   'image/source_id': tf.io.FixedLenFeature([], tf.string),
+    #   'image/encoded': tf.io.FixedLenFeature([], tf.string),
+    #   'image/format': tf.io.FixedLenFeature([], tf.string),
+    #   'image/object/bbox/xmin': tf.io.VarLenFeature(dtype=tf.float32),# tf.train.FloatList(value=value)
+    #   'image/object/bbox/xmax': tf.io.VarLenFeature(dtype=tf.float32),
+    #   'image/object/bbox/ymin': tf.io.VarLenFeature(dtype=tf.float32),
+    #   'image/object/bbox/ymax': tf.io.VarLenFeature(dtype=tf.float32),
+    #   'image/object/class/text': tf.io.VarLenFeature(dtype=tf.string),
+    #   'image/object/class/label': tf.io.VarLenFeature(dtype=tf.int64),
+    #   }
+
+    # def _parse_image_function(example_proto):
+    #   # Parse the input tf.train.Example proto using the dictionary above.
+    #   return tf.io.parse_single_example(example_proto, image_feature_description)
+
+    parsed_image_dataset = raw_image_dataset.map(parse_record)
     parsed_image_dataset
-
-    for image_features in parsed_image_dataset.take(3):
-        image_raw = image_features['image/encoded'].numpy()
-        box_x_mins = image_features['image/object/bbox/xmin'].values
-        tf.image.decode_image(image_raw)
-        plt.imshow(tf.image.decode_image(image_raw).numpy())
-        plt.show()
-        # display.display(display.Image(data=image_raw))
 
     # Debug Visu
     if False:
@@ -103,7 +114,7 @@ def parse_tfr_element(element):
     filename = content['image/filename']
     print(filename)
     label = content['image/object/class/label']
-    # raw_image = content['image/encoded']
+    raw_image = content['image/encoded']
 
     # get our 'feature'-- our image -- and reshape it appropriately
     feature = tf.io.parse_tensor(raw_image, out_type=tf.int16)
