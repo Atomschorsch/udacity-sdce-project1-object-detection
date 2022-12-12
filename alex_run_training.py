7bdd47f7819338bdbdff8e29bf2e12104d7e8937
Atomschorsch <a.klueber@mailbox.org>
2022-12-12 18:47:49 +0000

    Add animation

--- alex_run_training.py
+++ alex_run_training.py
@@ -7,14 +7,18 @@ if __name__ == "__main__":
     model_dir , model_config = create_new_config(experiments_folder = 'experiments')
 
     # Start tensorboard
+    # python -m tensorboard.main --logdir experiments
     subprocess.Popen(['python', '-m', 'tensorboard.main','--logdir', model_dir])
 
 
-    # Run training (~20 min on RTX4090)
+    # Run training (~20 min/25000 steps on RTX4090)
     # Reference training: python experiments/model_main_tf2.py --model_dir=experiments/reference/ --pipeline_config_path=experiments/reference/pipeline_new.config
     # Experiment training: python experiments/model_main_tf2.py --model_dir=experiments/experiment0/ --pipeline_config_path=experiments/experiment0/pipeline_new.config
     os.system(f"python experiments/model_main_tf2.py --model_dir={model_dir} --pipeline_config_path={model_config}")
 
-    # Evaluation (~ min on RTX4090)
+    # Evaluation
     # python experiments/model_main_tf2.py --model_dir=experiments/reference/ --pipeline_config_path=experiments/reference/pipeline_new.config --checkpoint_dir=experiments/reference/
     # python experiments/model_main_tf2.py --model_dir=experiments/experiment0/ --pipeline_config_path=experiments/experiment0/pipeline_new.config --checkpoint_dir=experiments/experiment0/
+
+    # Export
+    # python experiments/exporter_main_v2.py --input_type image_tensor --pipeline_config_path experiments/experiment10/pipeline_new.config --trained_checkpoint_dir experiments/experiment10/ --output_directory experiments/experiment10/exported/
